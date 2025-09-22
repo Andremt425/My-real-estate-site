13ba03772470b0e24dea6932842a6ee700871a38
Andremt425 <andretoerien425@gmail.com>
2025-09-22 14:03:33 +0200

    added database work

--- backend/models/listing.py
+++ backend/models/listing.py
@@ -1,6 +1,12 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, Float, create_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-class Listing(BaseModel):
-    title : str
-    description : str
-    price : str
+Base = declarative_base()
+
+class Listing(Base):
+    __tablename__ = "listings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
